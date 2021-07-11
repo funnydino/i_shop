29ad28bde0dfc55c138f04cfcd1658c3685f2bfc
Aleksey Ponomarev <dazed39@mail.ru>
2021-07-11 18:09:10 +0200

    move products list to additional component, some other fixes

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,7 @@ module.exports = {
     parser: 'babel-eslint',
   },
   rules: {
+    'quotes': ['error', 'single'],
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
   },
